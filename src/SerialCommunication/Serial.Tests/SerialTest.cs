2c59a54e1b5256f2e3d3bd50e04d7852d03a7323
angelinn <angelin.nedelchev@outlook.com>
2018-08-27 11:12:10 +0300

    Add first test.

--- src/SerialCommunication/Serial.Tests/SerialTest.cs
+++ src/SerialCommunication/Serial.Tests/SerialTest.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
+using Serial.Linux;
+using Serial.Linux.Native;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,9 +12,19 @@ namespace Serial.Tests
     public class SerialTest
     {
         [Test]
-        public void TestTrue()
+        public void SanityCheck()
         {
             Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void RunsWithExistingSerial()
+        {
+            Mock<ILibc> mock = new Mock<ILibc>();
+            mock.Setup(o => o.Open(String.Empty, Libc.OpenFlags.O_RDWR)).Returns(1);
+
+            SerialCommunication serial = new SerialCommunication(mock.Object);
+            serial.Open("dummy");
         }
     }
 }
