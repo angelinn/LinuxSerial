06588514b8bed8bdd93ca012897094b3e22c5316
angelinn <angelin.nedelchev@outlook.com>
2018-08-27 10:53:10 +0300

    Add nunit project.

---
+++ src/SerialCommunication/Serial.Tests/SerialTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serial.Tests
+{
+    [TestFixture]
+    public class SerialTest
+    {
+        [Test]
+        public void TestTrue()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
