ebe5b925bc4ab0762f4ace19f3adca31a8a723bc
angelinn <angelin.nedelchev@outlook.com>
2018-08-27 11:57:35 +0300

    Mock IntPtr string copying.

--- src/SerialCommunication/Serial.Tests/SerialTest.cs
+++ src/SerialCommunication/Serial.Tests/SerialTest.cs
@@ -5,7 +5,9 @@ using Serial.Linux.Exceptions;
 using Serial.Linux.Native;
 using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Serial.Tests
 {
@@ -36,6 +38,33 @@ namespace Serial.Tests
 
             SerialCommunication serial = new SerialCommunication(mock.Object);
             Assert.Throws<SerialNotFoundException>(() => serial.Open("dummy"));
+        }
+
+        [Test]
+        public void ReadsData()
+        {
+            string targetString = "OK";
+            byte[] bytes = Encoding.ASCII.GetBytes(targetString + '\0');
+
+            Mock<ILibc> mock = new Mock<ILibc>();
+            mock.Setup(o => o.Read(It.IsAny<int>(), It.IsAny<IntPtr>(), It.IsAny<IntPtr>()))
+                .Returns(new IntPtr(1))
+                .Callback<int, IntPtr, IntPtr>((i, p, c) => Marshal.Copy(bytes, 0, p, bytes.Length));
+
+            string received = String.Empty;
+            
+            SerialCommunication serial = new SerialCommunication(mock.Object);
+            serial.Open("dummy");
+            serial.OnDataReceived += (s, e) => received = e;
+
+            Task.Run(async () =>
+            {
+                await Task.Delay(1000);
+                serial.StopReading();
+            });
+            
+            serial.Read();
+            Assert.AreEqual(targetString, received);
         }
     }
 }
