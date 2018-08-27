babaf2126cd2a87f920e8e39d0699a5dedcd35cf
angelinn <angelin.nedelchev@outlook.com>
2018-08-27 11:17:49 +0300

    Add exceptions.

---
+++ src/SerialCommunication/Serial.Linux/Exceptions/SerialNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serial.Linux.Exceptions
+{
+    public class SerialNotFoundException : Exception
+    {
+        public SerialNotFoundException(string message = "") : base(message)
+        {   }
+    }
+}
