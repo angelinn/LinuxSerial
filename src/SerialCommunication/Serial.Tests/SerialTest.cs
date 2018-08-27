f3082533b6b4e22fa338172ec676555a0de97d9b
angelinn <angelin.nedelchev@outlook.com>
2018-08-27 11:17:43 +0300

    Add ThrowsExceptionOnNonExistingSerial test.

--- src/SerialCommunication/Serial.Tests/SerialTest.cs
+++ src/SerialCommunication/Serial.Tests/SerialTest.cs
@@ -1,6 +1,7 @@
 ﻿using Moq;
 using NUnit.Framework;
 using Serial.Linux;
+using Serial.Linux.Exceptions;
 using Serial.Linux.Native;
 using System;
 using System.Collections.Generic;
@@ -24,7 +25,17 @@ namespace Serial.Tests
             mock.Setup(o => o.Open(String.Empty, Libc.OpenFlags.O_RDWR)).Returns(1);
 
             SerialCommunication serial = new SerialCommunication(mock.Object);
-            serial.Open("dummy");
+            Assert.DoesNotThrow(() => serial.Open("dummy"));
+        }
+        
+        [Test]
+        public void ThrowsExceptionOnNonExistingSerial()
+        {
+            Mock<ILibc> mock = new Mock<ILibc>();
+            mock.Setup(o => o.Open(String.Empty, Libc.OpenFlags.O_RDWR)).Returns(-1);
+
+            SerialCommunication serial = new SerialCommunication(mock.Object);
+            Assert.Throws<SerialNotFoundException>(() => serial.Open("dummy"));
         }
     }
 }
